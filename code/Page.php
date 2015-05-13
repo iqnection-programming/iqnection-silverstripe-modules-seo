4bfc26a058659c7f0dd496a4ef9d32970f3a2b69
App1e5auce <mensch.cory@gmail.com>
2015-05-13 08:20:41 -0400

    Moving this in here with tricks

--- code/Page.php
+++ code/Page.php
@@ -30,6 +30,24 @@
 		{
 			return $this->owner->NoFollow ? "rel='nofollow'" : "";	
 		}
+		
+		public function hook_onBeforeWrite($owner)
+		{
+			if ($URLRedirects = $owner->URLRedirects)
+			{
+				$RedirectURLs = explode("\n",$URLRedirects);
+				$NewRedirectURLs = array();
+				foreach($RedirectURLs as $RedirectURL)
+				{
+					$NewRedirectURL = preg_replace("#".addslashes(Director::absoluteBaseURL())."#","",$RedirectURL);
+					// make sure there's a slash at the begining
+					if (substr($NewRedirectURL,0,1) != "/") $NewRedirectURL = "/".$NewRedirectURL;
+					str_replace("//","/",$NewRedirectURL);
+					$NewRedirectURLs[] = $NewRedirectURL;
+				}
+				$owner->URLRedirects = implode("\n",$NewRedirectURLs);
+			}
+		}
 	}
 	
 	class IQSEO_Page_Controller extends Extension {

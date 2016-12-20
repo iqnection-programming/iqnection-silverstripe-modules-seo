ad5a2426e066df99676391e279760f2dd0f454e2
iqmeckert <meckert@iqnection.com>
2016-12-20 15:11:54 -0500

    ## [1.0.1] - Added Additional Code fields for header and footer. To include tracking scripts and whatnot

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,5 +2,8 @@
 # Change Log
 
 
+## [1.0.1]
+- Added Additional Code fields for header and footer. To include tracking scripts and whatnot
+
 ## [1.0.0]
 - First stable build
---
+++ code/SiteConfig_AdditionalCode.php
@@ -0,0 +1,27 @@
+<?php
+
+class SiteConfig_AdditionalCode extends DataExtension 
+{
+
+	private static $db = array(
+		'AdditionalHeaderCode' => 'Text',
+		'AdditionalFooterCode' => 'Text',
+	);
+
+	public function updateCMSFields(FieldList $fields) 
+	{
+		if( permission::check('ADMIN') )
+		{
+			$fields->addFieldToTab('Root.AdditionalCode', CodeEditorField::create('AdditionalHeaderCode','Additional Header JS/CSS Code',50)
+				->addExtraClass('stacked')
+				->setRows(30)
+				->setMode('html')
+			);
+			$fields->addFieldToTab('Root.AdditionalCode', CodeEditorField::create('AdditionalFooterCode','Additional Footer JS/CSS Code',50)
+				->addExtraClass('stacked')
+				->setRows(30)
+				->setMode('html')
+			);
+		}
+	}
+}
